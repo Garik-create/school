47eded66e08e35f3b3412d1df22f931276873c23
Garik-create <pfdfkbybujhm@yandex.ru>
2023-02-14 01:55:46 +0300

    Выполнил д.з.

--- src/main/resources/liquibase/scripts/course-four-lesson-three.sql
+++ src/main/resources/liquibase/scripts/course-four-lesson-three.sql
@@ -1,3 +1,7 @@
 -- liquibase formatted sql
 
 -- changeset izavalin:1
+CREATE INDEX student_name_index ON student (name)
+
+-- changeset izavalin:2
+CREATE INDEX faculty_name_color_index ON faculty (name, color)
