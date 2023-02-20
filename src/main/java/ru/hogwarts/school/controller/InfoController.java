04572fe4619577b213f713592130db9d5214ae65
Garik-create <pfdfkbybujhm@yandex.ru>
2023-02-20 15:09:37 +0300

    Выполнил д.з. 4.5

--- src/main/java/ru/hogwarts/school/controller/InfoController.java
+++ src/main/java/ru/hogwarts/school/controller/InfoController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.RestController;
 import ru.hogwarts.school.service.InfoService;
 import ru.hogwarts.school.service.InfoServiceImpl;
 
+import java.util.stream.Stream;
+
 @RestController
 public class InfoController {
     public InfoController(InfoService infoService) {
@@ -15,11 +17,19 @@ public class InfoController {
     }
 
 
-private final InfoService infoService;
+    private final InfoService infoService;
 
     @GetMapping("/getPort")
     public ResponseEntity<Integer> getPort() {
         var port = infoService.getPort();
         return ResponseEntity.ok(port);
+    }
+
+    @GetMapping("/int")
+    public long getSum() {
+        return Stream.iterate(1L, a -> a +1)
+                .parallel()
+                .limit(1_000_000)
+                .reduce(0L, (a, b) -> a + b );
     }
 }
